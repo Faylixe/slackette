bb87c28649e863f0cec62628ca5f9c46f10fbd65
Félix Voituret <fvoituret@deezer.com>
2022-09-29 23:15:30 +0200

    🐛 run isort and black

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,24 +1,29 @@
 from slackette import *
 
+
 def test_block() -> None:
-    print(Blocks(blocks=[
-        Section(
-            text=Markdown(text="linl"),
-            accessory=Image(image_url="http://cover"),
-        ),
-        Actions(
-            elements=[
-                Button(
-                    style=Style.primary,
-                    text=PlainText(text="Approve"),
-                    value="validate"
+    print(
+        Blocks(
+            blocks=[
+                Section(
+                    text=Markdown(text="linl"),
+                    accessory=Image(image_url="http://cover"),
                 ),
-                Button(
-                    style=Style.danger,
-                    text=PlainText(text="Deny"),
-                    value="deny"
+                Actions(
+                    elements=[
+                        Button(
+                            style=Style.primary,
+                            text=PlainText(text="Approve"),
+                            value="validate",
+                        ),
+                        Button(
+                            style=Style.danger,
+                            text=PlainText(text="Deny"),
+                            value="deny",
+                        ),
+                    ]
                 ),
             ]
-        )
-    ]).dict())
+        ).dict()
+    )
     assert False
