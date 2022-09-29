a043a626e2cc482a6e081e0342f2aa8dd0b86919
Félix Voituret <fvoituret@deezer.com>
2022-09-29 09:41:43 +0200

    🚧  update security module

--- slackette/security.py
+++ slackette/security.py
@@ -1,6 +1,11 @@
 from hashlib import sha256
 from hmac import new as hmac
-from typing import Any, Callable, Mapping, Protocol, TypeVar, Union
+from typing import Any, Callable, Mapping, Protocol, TypeVar
+
+
+class SlackHeaders(object):
+    X_SLACK_SIGNATURE = "X-Slack-Signature"
+    X_SLACK_REQUEST_TIMESTAMP = "X-Slack-Request-Timestamp"
 
 
 class HeadersProtocol(Protocol):
@@ -17,8 +22,30 @@ Response = TypeVar("Response")
 Endpoint = Callable[[HeadersProtocol], Response]
 
 
+class InvalidSignatureError(ValueError):
+    pass
+
+
+def compute_slack_signature(
+    protocol: HeadersProtocol,
+    signing_secret: str,
+    version: str,
+) -> str:
+    """
+    """
+    timestamp = protocol.headers.get(SlackHeaders.X_SLACK_REQUEST_TIMESTAMP)
+    body = protocol.body()
+    message = f"{version}:{timestamp}:{body}"
+    signature = hmac(
+        bytes(signing_secret),
+        msg=bytes(message),
+        digestmod=sha256,
+    )
+    return signature.hexdigest()
+
+
 def SlackRequest(
-    signing_secret: Union[bytes, str],
+    signing_secret: str,
     version: str = "v0",
 ) -> Callable[[Endpoint], Endpoint]:
     """
@@ -28,18 +55,17 @@ def SlackRequest(
     See https://api.slack.com/authentication/verifying-requests-from-slack
     """
     def wrapper(endpoint: Endpoint) -> Endpoint:
-        def middleware(request: HeadersProtocol) -> Response:
+        def middleware(protocol: HeadersProtocol) -> Any:
             try:
-                expected = request.headers.get("X-Slack-Signature")
-                timestamp = request.headers.get("X-Slack-Request-Timestamp")
-                body = request.body()
-                message = bytes(f"{version}:{timestamp}:{body}")
-                if isinstance(signing_secret, str):
-                    signing_secret = bytes(signing_secret)
-                signature = hmac(signing_secret, msg=message, digestmod=sha256)
-                if signature.hexdigest() != expected:
-                    raise ValueError("Invalid signature")
-                return endpoint(request)
+                expected = protocol.headers.get(SlackHeaders.X_SLACK_SIGNATURE)
+                actual = compute_slack_signature(
+                    protocol,
+                    signing_secret,
+                    version,
+                )
+                if actual != expected:
+                    raise InvalidSignatureError()
+                return endpoint(protocol)
             except Exception as e:
                 # TODO: log
                 pass
